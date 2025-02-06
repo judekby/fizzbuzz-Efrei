b31773bc1486465031e18e9ab659287dd469d0f7
judekabeya <judek9392@gmail.com>
2025-02-06 11:09:59 +0100

    update

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -1,22 +1,27 @@
 import pytest
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import main  
 
-def multiple_3():
+def test_multiple_3():
     assert main.fizzbuzz(3) == "Fizz"
     assert main.fizzbuzz(6) == "Fizz"
     assert main.fizzbuzz(9) == "Fizz"
 
-def multiple_5():
+def test_multiple_5():
     assert main.fizzbuzz(5) == "Buzz"
     assert main.fizzbuzz(10) == "Buzz"
     assert main.fizzbuzz(20) == "Buzz"
 
-def multiple_3_and_5():
+def test_multiple_3_and_5():
     assert main.fizzbuzz(15) == "FizzBuzz"
     assert main.fizzbuzz(30) == "FizzBuzz"
     assert main.fizzbuzz(45) == "FizzBuzz"
 
-def not_multiple_3_et_5():
+def test_not_multiple_3_et_5():
     assert main.fizzbuzz(1) == 1
     assert main.fizzbuzz(2) == 2
     assert main.fizzbuzz(4) == 4
